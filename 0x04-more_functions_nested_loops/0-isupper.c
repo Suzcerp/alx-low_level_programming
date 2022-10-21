9e211926f0d8260e15572caacb60c3d1b5864b42
Umoh <suzcerp@gmail.com>
2022-10-21 00:36:00 -0700

    task 0 commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+** main - check the code
+** _isupper - a function that checks for uppercase character 
+** Return: Always 0.
+**/
+
+int main(void)
+
+{
+	
+	char c;
+	
+	if ((c >= 'A') && (c <= 'Z'))
+		
+		return (1);
+	else
+		
+	return (0);
+	
+}
